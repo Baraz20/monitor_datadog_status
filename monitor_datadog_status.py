48f63d8cb2d93794df415b24122e2b49f8b3d1af
Tal Baraz <Tal.orBS@Gmail.com>
2021-07-26 10:44:43 +0300

    implemt SetInterval

--- monitor_datadog_status.py
+++ monitor_datadog_status.py
@@ -1,6 +1,24 @@
 import argparse
 import requests
 import time
+import threading
+
+
+class SetInterval(threading.Thread):
+    def __init__(self, func, event, freq : int):
+        '''calls the @func function after @freq seconds
+        :param callback: callback function to invoke
+        :param event: external event for controlling the update operation
+        :param freq: time in seconds between each call of the @func function
+        '''
+        self.func = func
+        self.event = event
+        self.freq = freq
+        super(SetInterval, self).__init__()
+
+    def run(self):
+        while not self.event.wait(self.freq):
+            self.func()
 
 
 def monitor_datadog_status(freq: int) -> None:
@@ -19,14 +37,20 @@ def monitor_datadog_status(freq: int) -> None:
         for component in data["components"]:
             if "operational" not in component["status"]:
                 now = time.strftime("%d/%m/%Y %H:%M:%S")
-                print(f'{now} Component {component["name"]} is in status: {component["status"]}')
+                print(
+                    f'{now} Component {component["name"]} is in status: {component["status"]}')
 
     # The API's URL
     URL = "https://status.datadoghq.com/api/v2/components.json"
     start_time = int(time.time())
     # making a request every @freq seconds
     while 1:
-        process_datadog_status(requests.get(URL).json())
+        r = requests.get(URL)
+        if r.status_code == 200:
+            process_datadog_status(r.json())
+        else:
+            print("Faild to get API respond... retrying")
+        # accurate way to calculate time to sleep
         time.sleep(freq - (time.time() - start_time) % freq)
 
 
